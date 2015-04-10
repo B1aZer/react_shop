2e23b6ee78153beaa8d3a4307053f533f9c9cf0e
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2015-04-10 23:51:15 +0300

    Adding bower deps

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,13 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
+var mainBowerFiles = require('main-bower-files');
 
+var flatten = require('gulp-flatten');
+var gulpFilter = require('gulp-filter');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var mainBowerFiles = require('main-bower-files');
 
 var path = {
   JS: './react/main.jsx',
@@ -41,3 +47,39 @@ function bundle() {
     .pipe(gulp.dest(path.DEST));
 }
 
+gulp.task('copy', function() {
+
+  // Define paths variables
+  var dest_path =  'public';
+
+  var jsFilter = gulpFilter(['*.js', '*.js.map']);
+  var cssFilter = gulpFilter(['*.css', '*.css.map']);
+  var fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf']);
+
+  return gulp.src(mainBowerFiles())
+
+  // grab vendor js files from bower_components, minify and push in /public
+  .pipe(jsFilter)
+  .pipe(gulp.dest(dest_path + '/javascripts/vendor'))
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: ".min"
+  }))
+  .pipe(gulp.dest(dest_path + '/javascripts/vendor'))
+  .pipe(jsFilter.restore())
+
+  // grab vendor css files from bower_components, minify and push in /public
+  .pipe(cssFilter)
+  .pipe(gulp.dest(dest_path + '/stylesheets/vendor'))
+  .pipe(minifycss())
+  .pipe(rename({
+    suffix: ".min"
+  }))
+  .pipe(gulp.dest(dest_path + '/stylesheets/vendor'))
+  .pipe(cssFilter.restore())
+
+  // grab vendor font files from bower_components and push in /public 
+  .pipe(fontFilter)
+  .pipe(flatten())
+  .pipe(gulp.dest(dest_path + '/fonts'));
+});
--- react/main.jsx
+++ react/main.jsx
@@ -1,4 +1,5 @@
 /** @jsx React.DOM */
 var React = require('react');
 var App = require('./app.jsx');
+
 React.render(<App/>, document.getElementById('example'));
